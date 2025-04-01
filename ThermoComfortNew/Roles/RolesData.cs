65ed8271217a85dff9d860ee0bb844aaa5a5c5fb
100qn <100qn@DESKTOP-VL1VQRS>
2025-04-01 13:01:57 +0300

    Updated admin password

--- ThermoComfortNew/Roles/RolesData.cs
+++ ThermoComfortNew/Roles/RolesData.cs
@@ -53,8 +53,8 @@ namespace ThermoComfortNew.Roles
                     PhoneNumber = "0886688228"
                 };
 
-                //потребител с парола "password"
-                var result = userManager.CreateAsync(user, "password").Result;
+                //потребител с парола "tc12345"
+                var result = userManager.CreateAsync(user, "tc12345").Result;
 
                 if (result.Succeeded)
                 {
